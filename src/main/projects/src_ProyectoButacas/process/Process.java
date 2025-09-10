c268fb868b4f6a86d707e4d03b5e8e4fea3bc997
seumanito <paredesamedina@gmail.com>
2025-09-10 18:51:56 -0400

    Seguir modificando el Process

--- src/main/projects/src_ProyectoButacas/process/Process.java
+++ src/main/projects/src_ProyectoButacas/process/Process.java
@@ -1,5 +1,9 @@
 package main.projects.src_ProyectoButacas.process;
 
+import java.io.IOException;
+
+import cinema.Validate.Validate;
+
 public class Process {
     public static void iniMatrix(int[][][] cinema) {
         if (cinema != null) {
@@ -49,5 +53,30 @@ public class Process {
     public static final String[] AGE_CLASSIFICATIONS = {
             "Adulto General => Precio: 3$", "Niño [Hasta los 14 años] =>  Precio: 2$", "Adulto de la Tercera Edad => Precio: 1$"
     };
+
+    public static void dataEntry(String[] movieSchedule) throws IOException {
+        String text;
+        System.out.println("\nEl cine esta abierto desde las 12:00 PM hasta las 10:30 PM");
+        if (movieSchedule != null) {
+            for (int i = 0; i < movieSchedule.length; i++) {
+                text = "- Ingresa cual es el horario[" + (i+1) + "] disponible el dia de hoy. En formato (hh:mm PM): ";
+                movieSchedule[i] = Validate.valSchedule(text);
+            }
+        }
+    }
+    
+    public static void quantProcess(String[]usersQuant,int[][][] cinemaMatrix, String[] movieSchedule,int[]idRow) throws IOException {
+        String text,name="";
+        
+
+        for (int i = 0; i < usersQuant.length; i++) {
+            text = "- Ingrese el nombre del usuario [" + (i + 1) + "] en la fila: ";
+            usersQuant[i] = Validate.valString(name,text);
+            text="- Ingrese la cedula del usuario [" + (i + 1) + "] en la fila: ";
+            idRow[i]= Validate.valInt(text,35000000,400000);
+            purchaseProcessStart(cinemaMatrix,movieSchedule,usersQuant[i]);
+        }
+
+    }
 
 }
