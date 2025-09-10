6270d0d0160391efa100347893a8ddee75ff4f82
seumanito <paredesamedina@gmail.com>
2025-09-10 19:06:57 -0400

    Archivo ConsultMain con sus importaciones

---
+++ src/main/projects/src_ProyectoButacas/helpers/consultMain.java
@@ -0,0 +1,10 @@
+package main.projects.src_ProyectoButacas.helpers;
+
+
+import java.io.IOException;
+import java.util.Scanner;
+import src.main.projects.src_ProyectoButacas.objects.*;
+
+public class consultMain {
+    
+}
