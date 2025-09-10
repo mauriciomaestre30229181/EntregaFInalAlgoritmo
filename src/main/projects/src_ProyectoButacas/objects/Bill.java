e5ff197463988169dec47db1893b52bf58985f6c
seumanito <paredesamedina@gmail.com>
2025-09-10 18:58:49 -0400

    Subir Objectos Faltantes

---
+++ src/main/projects/src_ProyectoButacas/objects/Bill.java
@@ -0,0 +1,28 @@
+package main.projects.src_ProyectoButacas.objects;
+
+public class Bill {
+    private double bill;
+    private String  serialNumber;
+
+    public Bill(double bill, String serialNumber){
+        this.utilValBill(bill);
+        this.
+    }
+
+    public double getBill(){
+        return this.bill;
+    }
+
+    public void setBill(double bill){
+        this.utilValBill(bill);
+    }
+
+    private void utilValBill(double bill){
+        if (bill<0){
+            throw new IllegalArgumentException(" La cuenta no puede ser negativa. ");
+        }else{
+            this.bill=bill;
+        }
+    }
+    
+}
