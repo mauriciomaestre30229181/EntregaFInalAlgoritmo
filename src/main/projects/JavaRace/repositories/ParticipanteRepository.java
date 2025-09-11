74c75e7275dc1a2a9f3dd73acd6cb4076d16ba3b
freddyapds19 <freddyapds19@gmail.com>
2025-09-10 22:12:21 -0400

    Añado el proyecto JAVARACE

---
+++ src/main/projects/JavaRace/repositories/ParticipanteRepository.java
@@ -0,0 +1,9 @@
+package repositories;
+
+public interface ParticipanteRepository {
+    void save(Participante participante);
+    Participante findByNumero(int numeroParticipante);
+    Participante[] findAll();
+    void update(Participante participante);
+    void delete(int numeroParticipante);
+}
